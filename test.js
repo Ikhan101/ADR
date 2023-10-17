b1c4447fa0c30bb62a296d99de955cda21bb50a3
Ikhan101 <c1008166@my.shu.ac.uk>
2023-10-17 11:08:49 +0100

    index

---
+++ test.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("btn");
+const output = document.getElementById("Output");
+
+button.addEventListener("click", function() {
+    output.textContent = "mi piacciono i ragazzi";
+});
